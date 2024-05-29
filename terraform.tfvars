7af7b318ac76c8cb694a4ac7fc803b2a13949f15
zakaria saif <129240583+zacksfF@users.noreply.github.com>
2024-05-29 15:19:30 +0100

    add new one

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "<galvanized-vent-417914>"
+region = "us-central1"
---
+++ version.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "4.27.0"
+    }
+  }
+  required_version = "v1.7.5"
+}
